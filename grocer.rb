292f6589e95654ba02a47fe8822c4fc3eaf277c5
Robinson Santana Silverio <robinsntn@gmail.com>
2019-10-07 10:51:36 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,14 @@
 def consolidate_cart(cart)
-  
 end
 
 def apply_coupons(cart, coupons)
-  
+  # code here
 end
 
 def apply_clearance(cart)
-  
+  # code here
 end
 
 def checkout(cart, coupons)
-  
+  # code here
 end
